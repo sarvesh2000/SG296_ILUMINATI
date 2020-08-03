38f309a49a21d5a29a9f3e9a1c27b48f092c210c
Sarvesh <sarvesh4232@gmail.com>
2020-08-03 14:36:12 +0530

    Shows Users List

--- js/loadUsers.js
+++ js/loadUsers.js
@@ -1,4 +1,27 @@
 $(document).ready(function(){
     var query = new Parse.Query("User");
-    query
+    query.ascending("objectId");
+    query.find().then((result)=>{
+        result.forEach(element => {
+            var tr = $('<tr>');
+            $("#myContent").append(tr);
+            $(tr).append('<td>' + element.id + '</td>');
+            $(tr).append('<td>' + element.getUsername() + '</td>');
+            $(tr).append('<td>' + element.get('officerClass') + '</td>');
+            $(tr).append('<td>' + element.get('accountStatus') + '</td>');
+            if(typeof element.get('email') === 'undefined')
+                $(tr).append('<td>Not Registered</td>');
+            else
+            $(tr).append('<td>' + element.get('email') + '</td>');
+            if(typeof element.get('phoneNumber') === 'undefined')
+                $(tr).append('<td>Not Registered</td>');
+            else
+                $(tr).append('<td>' + element.get('phoneNumber') + '</td>');
+            if(typeof element.get('contactDetails') === 'undefined')
+                $(tr).append('<td>Not Registered</td>');
+            else
+                $(tr).append('<td>' + element.get('contactDetails') + '</td>');
+            $('#myTable').DataTable();
+        });
+    });
 });
