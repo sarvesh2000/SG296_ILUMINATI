78677233bafc7e584251e2585fcb358ce6ad2afb
Sarvesh <sarvesh4232@gmail.com>
2020-08-03 15:09:07 +0530

    Changes for showing file data

--- js/loadFiles.js
+++ js/loadFiles.js
@@ -8,25 +8,27 @@ $(document).ready(function(){
             var FileID = element.get('FileID');
             query2.equalTo('FileID',FileID);
             query2.find().then((res)=>{
-                var assignedTo = res[0].get('AssignedTo');
-                var status = res[0].get('Action');
-                var tr = $('<tr>');
-                $("#myContent").append(tr);
-                $(tr).append('<td>' + element.get('FileID') + '</td>');
-                $(tr).append('<td>' + element.get('PersonName') + '</td>');
-                $(tr).append('<td>' + element.get('Priority') + '</td>');
-                $(tr).append('<td>' + element.get('Address') + '</td>');
-                $(tr).append('<td>' + element.get('ProblemDescription') + '</td>');
-                $(tr).append('<td>' + element.get('Category') + '</td>');
-                $(tr).append('<td>' + element.get('AssignmentType') + '</td>');
-                if(typeof element.get('CustomHierarchy') === 'undefined')
-                    $(tr).append('<td>None</td>');
-                else
-                    $(tr).append('<td>' + element.get('CustomHierarchy') + '</td>');
-                $(tr).append('<td>' + element.get('DeliveryDate') + '</td>');
-                $(tr).append('<td>' + assignedTo + '</td>');
-                $(tr).append('<td>' + status + '</td>');
-                $('#myTable').DataTable();
+                res.forEach(function(item){
+                    var assignedTo = item.get('AssignedTo');
+                    var status = item.get('Action');
+                    var tr = $('<tr>');
+                    $("#myContent").append(tr);
+                    $(tr).append('<td>' + element.get('FileID') + '</td>');
+                    $(tr).append('<td>' + element.get('PersonName') + '</td>');
+                    $(tr).append('<td>' + element.get('Priority') + '</td>');
+                    $(tr).append('<td>' + element.get('Address') + '</td>');
+                    $(tr).append('<td>' + element.get('ProblemDescription') + '</td>');
+                    $(tr).append('<td>' + element.get('Category') + '</td>');
+                    $(tr).append('<td>' + element.get('AssignmentType') + '</td>');
+                    if(typeof element.get('CustomHierarchy') === 'undefined')
+                        $(tr).append('<td>None</td>');
+                    else
+                        $(tr).append('<td>' + element.get('CustomHierarchy') + '</td>');
+                    $(tr).append('<td>' + element.get('DeliveryDate') + '</td>');
+                    $(tr).append('<td>' + assignedTo + '</td>');
+                    $(tr).append('<td>' + status + '</td>');
+                    $('#myTable').DataTable();
+                });
             });
         });
     });
